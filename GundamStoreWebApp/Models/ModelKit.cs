a1f9cda64b003b4c4b2be389ef7f1364c73a8b45
hyan <lucas.hyan@hotmail.com>
2023-11-09 16:39:02 -0300

    card basico feito

---
+++ GundamStoreWebApp/Models/ModelKit.cs
@@ -0,0 +1,14 @@
+﻿namespace GundamStoreWebApp.Models
+{
+    public class ModelKit
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string ImageUri { get; set; }
+        public double Price { get; set; }
+        public string ModelGrade { get; set; }
+        public DateTime RegistrationDate { get; set; }
+
+    }
+}
